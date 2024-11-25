4001342013c9d58812bd1ff6d54547ade8db189b
FaisalB71 <f9.k.lilz71@gmail.com>
2024-11-25 12:36:03 +0300

    f

---
+++ student.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+//1
+ class one 
+{
+
+    public void classY(){
+
+    
+    string name ="faisal";
+    
+    int age = 19;
+    
+   char grade ='B';
+
+string name2 = "bandr";
+
+int age2 = 18;
+
+char grade2 ='A';
+
+string name3 = "naif";
+int age3 = 21;
+
+char grade3 ='c';
+
+    
+Console.WriteLine(name);
+Console.WriteLine(name2);
+Console.WriteLine(name3);
+Console.WriteLine(age);
+Console.WriteLine(age2);
+Console.WriteLine(age3);
+Console.WriteLine(grade);
+Console.WriteLine(grade2);
+Console.WriteLine(grade3);
+    }
+
+
+
+}
+
+class w{
+// السوال الخامس
+public void wq(){
+    Console.Write("1 or 7");
+    
+int a = 1;
+    int a1 = int.Parse(Console.ReadLine());
+    if(a < 7)
+    Console.WriteLine("7");
+    
+     else
+    
+    Console.WriteLine("1");
+    
+    
+}}
+
+// السوال 2
+class e{
+static void Main(string[] args){
+int[] n1 = new int[6];
+for(int i = 0; 1 < 6; ++i)
+{
+    Console.WriteLine(i);
+    
+} 
+
+}}
+
+
+
+
+
